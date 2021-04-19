03792ecceec79583f739320cb7782944a8f40b6f
tuffnerdstuff <tuffnerdstuff@gmail.com>
2021-04-19 23:50:35 +0200

    Added comments

--- mapper/config.go
+++ mapper/config.go
@@ -1,5 +1,6 @@
 package mapper
 
+// NotificationConfig holds the configuration for the eMail notification about new Hauk sessions
 type NotificationConfig struct {
 	Enabled bool
 	Host    string
--- mqtt/message.go
+++ mqtt/message.go
@@ -1,5 +1,6 @@
 package mqtt
 
+// Message is an MQTT message consisting of a topic and a body (map)
 type Message struct {
 	Topic string
 	Body  map[string]interface{}
